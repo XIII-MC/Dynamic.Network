74b361817ab7cde53677bde2e3180661c3cb1481
XIII-MC <NA@gmail.com>
2023-12-04 20:21:57 +0100

    Preparing V2
    Signed-off-by: XIII-MC <NA@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,107 +1,157 @@
+
+import com.sun.jna.Function;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.PrintStream;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class Main {
+public class Main extends Colors {
 
-    public static List<String> argList = new ArrayList<>();
+    /*
+    Settings
+     */
+    // Maximum amount of threads for the net scan
+    public static int maxNetThreads = 256;
 
-    public static void main(String[] args) {
+    // How long should we wait for an answer from an ICMP ping
+    public static int maxICMPWait = 10000;
 
-        if (args.length > 0) {
-            for (final Object arg : Arrays.stream(args).toArray()) {
-                argList.add(arg.toString());
-            }
+    //Common ports to scan for exploits
+    public static List<Integer> commonPortList = new ArrayList<>();
 
-            System.out.println(argList.toString().contains("dport=true"));
+    public static void main(String[] args) throws IOException {
 
-            if (argList.toString().contains("scan=") || argList.contains("help")) {
+        // Temporarily enable ANSI support for Windows
+        if (System.getProperty("os.name").startsWith("Windows")) enableWindows10AnsiSupport();
 
-                if (args[0].contains("scan=")) {
-                    checkOpenPorts(args[0].replace("scan=", ""), argList.toString().contains("dport=true"));
-                } else if (args[0].contains("help")) {
-                    System.out.println(" ");
-                    System.out.println("Welcome to Dynamic.Network (DCN) vD1!");
-                    System.out.println("arg 0 = help | Show this page");
-                    System.out.println("arg 0 = scan=<IP> | Scan a precise IP scope");
-                    System.out.println("arg 0-99 = auto=true | Automatically start a port scan whenever an IP is reachable in your network.");
-                    System.out.println("arg 0-99 = dport=true | Scan the dynamic ports range (49152-65535)");
-                    System.out.println("No args | Basic network scan without port scanning");
-                    System.out.println(" ");
-                }
-            } else getNetworkIPs();
-        } else getNetworkIPs();
+        // Initialize settings
+        commonPortList.add(21); // FTP
+        commonPortList.add(22); // SSH
+        commonPortList.add(25); // SMTP
+        commonPortList.add(80); // HTTP
+        commonPortList.add(135); // RPC
+        commonPortList.add(139); // NetBIOS, Printer & Sharing
+        commonPortList.add(443); // HTTPS
+        commonPortList.add(445); // SMB
+        commonPortList.add(515); // Printer
+        commonPortList.add(808); // MS Sharing Services
+        commonPortList.add(3389); // RDP
+        commonPortList.add(9100); // Printer | HP JetDirect
+
+        // Prepare to enumerate net interfaces
+        final List<String> networkInterfaces = new ArrayList<>();
+        System.out.println(BLACK_BOLD_BRIGHT + WHITE_BACKGROUND + "[sys]" + RESET + " | Network interface lookup...");
+
+        // Grab and list net interfaces
+        final Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while(interfaces.hasMoreElements()) {
+            final NetworkInterface networkInterface = interfaces.nextElement();
+            if (!networkInterface.getInterfaceAddresses().isEmpty()) networkInterfaces.add(BLACK_BOLD_BRIGHT + WHITE_BACKGROUND + "[sys]" + RESET + " | Interface #" + networkInterfaces.size() + " : '" + networkInterface.getDisplayName() + "' | '" + networkInterface.getInterfaceAddresses().get(0) + "'");
+        }
+
+        // Print net interfaces
+        networkInterfaces.forEach(System.out::println);
+
+        // Ask which net interface to use (according to the arraylist index)
+        final Scanner scannerIntID = new Scanner(System.in);
+        System.out.println(BLACK_BOLD_BRIGHT + WHITE_BACKGROUND + "[sys]" + RESET + " | Please type the interface's ID that you would like lookup: ");
+        final int interfaceID = Integer.parseInt(scannerIntID.nextLine());
+
+        // Extract the IP from the interface name + IP output
+        final String interfaceIP = networkInterfaces.get(interfaceID).replaceAll(".*?(?<='/)", "").replaceAll("\\[.*", "").replaceAll("/.*", "");
+
+        // Logging
+        System.out.println(BLACK_BOLD_BRIGHT + WHITE_BACKGROUND + "[net]" + RESET + " | Starting network scan on '" + interfaceIP + "'...");
+
+        // Start net scan
+        getNetworkIPs(interfaceIP, 0);
     }
 
-    private static void getNetworkIPs() {
+    private static void getNetworkIPs(final String ipv4, final int mask) {
 
         final long startTime = System.currentTimeMillis();
 
-        final ExecutorService executorService = Executors.newFixedThreadPool(254);
+        // To make the task faster (and by a lot), we're going to use multithreading, 256 threads because 256 IPs (0-255)
+        final ExecutorService executorService = Executors.newFixedThreadPool(maxNetThreads);
         final List<CompletableFuture<Void>> futures = new ArrayList<>();
 
-        try {
+        // Split the IP, so we can change the last byte
+        final String[] ipv4Split = ipv4.split("\\.");
+        final String ip = ipv4Split[0] + "." + ipv4Split[1] + "." + ipv4Split[2] + ".";
 
-            final byte[] ip = InetAddress.getLocalHost().getAddress();
+        for (int i = 0; i <= 254; i++) {
 
-            for (int i = 0; i <= 254; i++) {
-
-                int finalI = i;
-
-                CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-                    try {
-
-                        ip[3] = (byte) finalI;
-                        final InetAddress address = InetAddress.getByAddress(ip);
-                        final String convIP = address.toString().substring(1);
-
-                        if (address.isReachable(5000)) {
-                            System.out.println("Host '" + convIP + "' is reachable");
-                            if (!argList.isEmpty() && argList.toString().contains("auto=true")) checkOpenPorts(convIP, argList.toString().contains("dport=true"));
-                        }
-                    } catch (final IOException ignored) {
-                    }
-                }, executorService);
-                futures.add(future);
-            }
-            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-            executorService.shutdown();
-            System.out.println("Finished network scan in " + (System.currentTimeMillis() - startTime) + "ms!");
-
-        } catch (final UnknownHostException ignored) {
-        }
-    }
-
-    private static void checkOpenPorts(final String ip, final boolean dynamicPorts) {
-
-        final long startTime = System.currentTimeMillis();
-
-        System.out.println("          Starting port scan on '" + ip + "'... (AUTO " + (dynamicPorts ? "65535" : "49152") + " THREADS)");
-
-        final ExecutorService executorService = Executors.newFixedThreadPool(65535);
-        List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-        for (int i = 0; i <= (dynamicPorts ? 65535 : 49152); i++) {
             int finalI = i;
-            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+
+            // Start task on a new thread
+            final CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+
                 try {
-                    new Socket().connect(new InetSocketAddress(ip, finalI), 1000);
-                    System.out.println("          Port '" + finalI + "' is open on '" + ip + "'");
-                } catch (final IOException ignored) {
-                }
+                    // According to Oracle this works similarly to an ICMP ping
+                    final InetAddress address = InetAddress.getByName(ip + finalI);
+                    if (address.isReachable(maxICMPWait)) {
+
+                        //Do a pre basic port scan to see if any port is vulnerable
+                        //Also establish which OS/Service the host is running
+                    }
+                    checkOpenPorts(ip + finalI, commonPortList);
+                } catch (final IOException ignored) {}
+
             }, executorService);
             futures.add(future);
         }
+
+        // Clear up all the threads
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-        executorService.shutdown();
-        System.out.println("          Port scan finished on '" + ip + "' in " + (System.currentTimeMillis() - startTime) + "ms!");
+        executorService.shutdownNow();
+
+        // Logging
+        System.out.println(BLACK_BOLD_BRIGHT + WHITE_BACKGROUND + "[sys]" + RESET + " | Finished network scan in " + (System.currentTimeMillis() - startTime) + "ms!");
+
+    }
+
+    private static void checkOpenPorts(final String ip, final List<Integer> ports) {
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(ports.size());
+        final List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+        ports.forEach(i -> {
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+
+                try {
+                    new Socket().connect(new InetSocketAddress(ip, i), 1000);
+                    System.out.println(GREEN + "✓" + WHITE + " | Port " + i + " is open on '" + ip + "'");
+                } catch (final IOException ignored) {
+                    //System.out.println(ANSI_RED + "⬤" + ANSI_RESET + " | Port " + i + " is closed.");
+                }
+            }, executorService);
+            futures.add(future);
+        });
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        executorService.shutdownNow();
+    }
+
+    private static void enableWindows10AnsiSupport() {
+        Function GetStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");
+        WinDef.DWORD STD_OUTPUT_HANDLE = new WinDef.DWORD(-11);
+        WinNT.HANDLE hOut = (WinNT.HANDLE) GetStdHandleFunc.invoke(WinNT.HANDLE.class, new Object[]{STD_OUTPUT_HANDLE});
+
+        WinDef.DWORDByReference p_dwMode = new WinDef.DWORDByReference(new WinDef.DWORD(0));
+        Function GetConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");
+        GetConsoleModeFunc.invoke(WinDef.BOOL.class, new Object[]{hOut, p_dwMode});
+
+        int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;
+        WinDef.DWORD dwMode = p_dwMode.getValue();
+        dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        Function SetConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");
+        SetConsoleModeFunc.invoke(WinDef.BOOL.class, new Object[]{hOut, dwMode});
     }
 }
