4853111337946b403d979c9dc1315d256d285314
XIII-MC <cyjallet@gmail.com>
2024-01-05 12:19:03 +0100

    new progressbar

--- src/utils/ProgressBarUtils.java
+++ src/utils/ProgressBarUtils.java
@@ -1,5 +1,10 @@
 package utils;
 
+import net.NetScan;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 public class ProgressBarUtils {
 
     public static void progressPercentage(int remain, int total) {
@@ -17,9 +22,36 @@ public class ProgressBarUtils {
             bareDone.append(icon);
         }
         String bareRemain = bare.substring(remainProcent, bare.length());
-        System.out.print("\r" + bareDone + bareRemain + " " + remainProcent + "%");
+        System.out.print("\r" + bareDone + bareRemain + " " + remainProcent + "% | Alive hosts: " + NetScan.aliveHost + " | Dead Hosts: " + NetScan.deadHost + " | Unknown Hosts: " + NetScan.unknownHost);
         if (remain == total) {
             System.out.print("\r");
         }
+    }
+
+    public static void printProgress(long startTime, long total, long current) {
+        long eta = current == 0 ? 0 :
+                (total - current) * (System.currentTimeMillis() - startTime) / current;
+
+        String etaHms = current == 0 ? "N/A" :
+                String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(eta),
+                        TimeUnit.MILLISECONDS.toMinutes(eta) % TimeUnit.HOURS.toMinutes(1),
+                        TimeUnit.MILLISECONDS.toSeconds(eta) % TimeUnit.MINUTES.toSeconds(1));
+
+        StringBuilder string = new StringBuilder(140);
+        int percent = (int) (current * 100 / total);
+        string
+                .append('\r')
+                .append(String.join("", Collections.nCopies(percent == 0 ? 2 : 2 - (int) (Math.log10(percent)), " ")))
+                .append(String.format(" %d%% [", percent))
+                .append(String.join("", Collections.nCopies(percent, "=")))
+                .append('>')
+                .append(String.join("", Collections.nCopies(100 - percent, " ")))
+                .append(']')
+                .append(String.join("", Collections.nCopies(current == 0 ? (int) (Math.log10(total)) : (int) (Math.log10(total)) - (int) (Math.log10(current)), " ")))
+                .append(String.format(" %d/%d, ETA: %s", current, total, etaHms));
+
+        if (percent == 100) {
+            System.out.print("\r");
+        } else System.out.print(string);
     }
 }
