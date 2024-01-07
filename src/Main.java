bfa90d684e6eeb036420fb36bf8bfadee003830a
XIII-MC <NA@gmail.com>
2024-01-07 20:42:08 +0100

    ARP update
    Signed-off-by: XIII-MC <NA@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,8 @@ import utils.ColorsUtils;
 import utils.InputUtils;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main extends Settings {
 
--- src/net/NetScan.java
+++ src/net/NetScan.java
@@ -1,6 +1,5 @@
 package net;
 
-import com.sun.javaws.progress.Progress;
 import options.Settings;
 import utils.ProgressBarUtils;
 
@@ -8,9 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -20,11 +17,10 @@ import java.util.regex.Pattern;
 
 public final class NetScan extends Settings {
 
-    private static DatagramSocket socket = null;
     private static int scannedIpCount = 0;
     public static int aliveHost = 0, deadHost = 0, unknownHost = 0;
 
-    public static HashMap<String, String> getNetworkIPs(final String ipv4, final int mask, final boolean reverse) {
+    public static HashMap<String, String> getNetworkIPs(final String ipv4, final int mask, final boolean reverse) throws IOException {
 
         final long startTime = System.currentTimeMillis();
 
@@ -38,6 +34,9 @@ public final class NetScan extends Settings {
 
         // Define the list and if it should be reversed
         final HashMap<String, String> returnIP = new HashMap<>();
+
+        // Pre cache the ARP list
+        final Matcher arpCache = getArpCache(ipv4);
 
         for (int i = 0; i <= 254; i++) {
 
@@ -54,7 +53,8 @@ public final class NetScan extends Settings {
                     // According to Oracle this works similarly to an ICMP ping
                     final InetAddress address = InetAddress.getByName(fullIp);
 
-                    broadcast("ARP_DISCOVERY", InetAddress.getByName(fullIp));
+                    // Force network discovery
+                    new Socket().connect(new InetSocketAddress(address, 10), 500);
 
                     // ARP
                     final String arpResult = getFromARP(fullIp);
@@ -63,12 +63,13 @@ public final class NetScan extends Settings {
 
                         if (!reverse) {
                             aliveHost++;
+
                             final String hostName =
                                     (Objects.equals(address.getHostName(), address.getHostAddress())
                                             ? "N/A"
                                             : address.getHostName());
                             returnIP.put(address.getHostAddress(),
-                                    hostName + " |" + (address.getHostAddress().length() > 12 ? (new String(new char[14 - address.getHostAddress().length()]).replace("\0", " ")) : "") + (new String(new char[(Math.max(hostName.length(), 8)) - hostName.length()]).replace("\0", " ")) + arpResult);
+                                    hostName + (new String(new char[(25 + fullIp.length()) - (hostName.length() + fullIp.length())]).replace("\0", " ")) + " | " + (new String(new char[fullIp.length() - 7]).replace("\0", " ")) +  arpResult);
                         }
                     } else {
 
@@ -95,6 +96,7 @@ public final class NetScan extends Settings {
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         executorService.shutdownNow();
 
+        System.out.print("\r");
 
         // Logging
         System.out.println("    | Finished network scan in " + (System.currentTimeMillis() - startTime) + "ms!");
@@ -112,9 +114,8 @@ public final class NetScan extends Settings {
             final InputStream is = process.getInputStream();
             final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-            String line = null;
+            String line;
             while ((line = reader.readLine()) != null) {
-                //System.out.println(line);
                 if ((line.contains("bytes=") && line.contains("time=") && line.contains("TTL="))) return true;
             }
 
@@ -135,7 +136,7 @@ public final class NetScan extends Settings {
             final InputStream is = process.getInputStream();
             final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-            String line = null;
+            String line;
             while ((line = reader.readLine()) != null) {
                 if (line.contains("No ARP Entries Found.")) return false;
             }
@@ -149,21 +150,43 @@ public final class NetScan extends Settings {
 
     public static String getFromARP(final String ipv4) throws IOException {
         final Scanner s = new Scanner(Runtime.getRuntime().exec("arp -a " + ipv4).getInputStream()).useDelimiter("\\A");
-        final Pattern macPattern = Pattern.compile("(?<=         ).*(?=     )");
+        final Pattern macPattern = Pattern.compile("(?<= {9}).*(?= {5})");
         final Matcher macMatcher = macPattern.matcher(s.hasNext() ? s.next() : "");
         if (macMatcher.find()) return macMatcher.group();
         return "N/A";
     }
 
+    public static Matcher getArpCache(final String ipv4) throws IOException {
+
+        final Scanner s = new Scanner(Runtime.getRuntime().exec("arp -a " + ipv4).getInputStream()).useDelimiter("\\A");
+
+        final Pattern ipPattern = Pattern.compile("\\d.*(?= {5}dynamic)");
+        final Matcher ipMatcher = ipPattern.matcher(s.hasNext() ? s.next() : "");
+
+        if (ipMatcher.find()) return ipMatcher;
+
+        return null;
+    }
+
+    public static String getMacFromArp(final String arpLine) throws IOException {
+
+        final Pattern macPattern = Pattern.compile("(?<= {11}).*");
+        final Matcher macMatcher = macPattern.matcher(arpLine);
+
+        if (macMatcher.find()) return macMatcher.group();
+
+        return "N/A";
+    }
+
     public static void broadcast(
-            String broadcastMessage, InetAddress address) throws IOException {
-        socket = new DatagramSocket();
+            final String broadcastMessage, InetAddress address) throws IOException {
+        final DatagramSocket socket = new DatagramSocket();
         socket.setBroadcast(true);
 
         byte[] buffer = broadcastMessage.getBytes();
 
-        DatagramPacket packet
-                = new DatagramPacket(buffer, buffer.length, address, 0);
+        final DatagramPacket packet
+                = new DatagramPacket(buffer, buffer.length, address, 4445);
         socket.send(packet);
         socket.close();
     }
